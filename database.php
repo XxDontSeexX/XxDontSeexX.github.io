8213a1291ccf9847520c5a0489e0b99fb1aaae0d
XxDontSeexX <129895753+XxDontSeexX@users.noreply.github.com>
2023-04-04 21:10:23 +0200

    Update database.php

--- database.php
+++ database.php
@@ -5,5 +5,5 @@ $gender=$_POST['gender'];
 $sql="INSERT INTO namegender(name,gender) VALUES('$name','$gender')";
 $result=mysqli_query($con,$sql);
 if($result=="true"){
- echo "<center><h2>Submitted successfully..</h2></center>";
+ echo "<center><h2>Se Envió A La Base De Datos hablé con AblayeYT#5922 para saber si se envió su mensaje ..</h2></center>";
 }
